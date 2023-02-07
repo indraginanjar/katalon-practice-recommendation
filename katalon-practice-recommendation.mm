7a6f15e40614d1f80b0cbcccf19f18dad30a4d7c
Indra Ginanjar <indraginanjar@gmail.com>
2023-02-07 18:20:41 +0700

    Katalon's practice recommendations

---
+++ katalon-practice-recommendation.mm
@@ -0,0 +1,211 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Katalon" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1674732430512" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Feature file" POSITION="right" ID="ID_573150991" CREATED="1674732433464" MODIFIED="1674732443846">
+<edge COLOR="#ff0000"/>
+<node TEXT="Scenario" ID="ID_386138073" CREATED="1674732445074" MODIFIED="1674732460925">
+<node TEXT="Tidak memiliki Examples atau sumber data" ID="ID_442973591" CREATED="1674732484326" MODIFIED="1674732516200"/>
+</node>
+<node TEXT="Scenario Outline" ID="ID_978110225" CREATED="1674732461809" MODIFIED="1674732468859">
+<node TEXT="Examples" ID="ID_244595711" CREATED="1674732471050" MODIFIED="1674732475175"/>
+<node TEXT="nama lain untuk" ID="ID_1342533257" CREATED="1674790722605" MODIFIED="1674790733958">
+<node TEXT="Scenario Template" ID="ID_1419005355" CREATED="1674790734548" MODIFIED="1674790741221"/>
+</node>
+</node>
+</node>
+<node TEXT="Recommendation" POSITION="left" ID="ID_329287003" CREATED="1674732546325" MODIFIED="1674732562093">
+<edge COLOR="#0000ff"/>
+<node TEXT="Structure" ID="ID_73827374" CREATED="1675592380544" MODIFIED="1675592383511">
+<node TEXT="root" ID="ID_731241855" CREATED="1674732566508" MODIFIED="1674732568992">
+<node TEXT="Test Cases" ID="ID_1515452753" CREATED="1674732571875" MODIFIED="1674732577074">
+<node TEXT="Scenario" ID="ID_1742259210" CREATED="1674732578003" MODIFIED="1674732583586">
+<node TEXT="Structural" ID="ID_1626439348" CREATED="1674733227869" MODIFIED="1675734170901"/>
+<node TEXT="Behavioral" ID="ID_1718376001" CREATED="1674733234344" MODIFIED="1674733239575">
+<node TEXT="Runner for each scenario defined in feature files" ID="ID_513959122" CREATED="1675601169092" MODIFIED="1675601257211">
+<icon BUILTIN="links/file/generic"/>
+</node>
+</node>
+</node>
+<node TEXT="Case" ID="ID_721993046" CREATED="1674732584127" MODIFIED="1674732585994"/>
+</node>
+<node TEXT="Test Suites" ID="ID_213221898" CREATED="1674733221700" MODIFIED="1674733225562">
+<node TEXT="Structural" ID="ID_1894061008" CREATED="1674733227869" MODIFIED="1675734115022">
+<node TEXT="Suite" ID="ID_712082125" CREATED="1674790894554" MODIFIED="1674790992562"/>
+<node TEXT="Scenario" ID="ID_1750309119" CREATED="1674790914050" MODIFIED="1674790967553"/>
+</node>
+<node TEXT="Behavioral" ID="ID_1831708173" CREATED="1674733234344" MODIFIED="1674733239575"/>
+<node TEXT="Collection" ID="ID_382928165" CREATED="1674733243866" MODIFIED="1674733246218"/>
+</node>
+<node TEXT="Data" ID="ID_436830363" CREATED="1674733270551" MODIFIED="1674733271895">
+<node TEXT="data_file.csv" ID="ID_1597427144" CREATED="1674733273164" MODIFIED="1674733305661"/>
+</node>
+</node>
+</node>
+<node TEXT="BDD" ID="ID_1333177508" CREATED="1675592398046" MODIFIED="1675592399641">
+<node TEXT="Groovy scripts" ID="ID_1205457677" CREATED="1675592400870" MODIFIED="1675592431652">
+<node TEXT="Don&apos;t use CucumberKW" ID="ID_1301177681" CREATED="1675592432294" MODIFIED="1675592446665"/>
+<node TEXT="Use other class methods" ID="ID_601283397" CREATED="1675592448434" MODIFIED="1675592472540"/>
+</node>
+<node TEXT="Cucumber" ID="ID_919571786" CREATED="1675600395623" MODIFIED="1675600400472">
+<node TEXT="Alway use double quote or single quote for parameters" ID="ID_496755994" CREATED="1675600401278" MODIFIED="1675600435439">
+<node TEXT="eg" ID="ID_1802133884" CREATED="1675600436467" MODIFIED="1675600453309">
+<node TEXT="I login with &quot;&lt;username&gt;&quot; and &quot;&lt;password&gt;&quot;" ID="ID_303719215" CREATED="1675600453941" MODIFIED="1675600482590"/>
+</node>
+<node TEXT="dont forget to escape double quote on related groovy method" ID="ID_241509703" CREATED="1675600489707" MODIFIED="1675600514561">
+<node TEXT="eg" ID="ID_1560773913" CREATED="1675600515868" MODIFIED="1675600520463">
+<node TEXT="@When(&quot;I login with \&quot;(.*)\&quot; and \&quot;(.*)\&quot;&quot;)" ID="ID_1936480591" CREATED="1675600520817" MODIFIED="1675600585602"/>
+</node>
+</node>
+<node TEXT="reason" ID="ID_8095121" CREATED="1675600592274" MODIFIED="1675600593802">
+<node TEXT="It&apos;s hard to spot empty string value parameter" ID="ID_475959788" CREATED="1675600600417" MODIFIED="1675600636794"/>
+<node TEXT="It&apos;s easier to differentiate parameter from the rest of statement" ID="ID_25148219" CREATED="1675600643830" MODIFIED="1675600703791"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Test case" ID="ID_1053344759" CREATED="1675592700461" MODIFIED="1675592770528">
+<node TEXT="Separated open browser" ID="ID_924570667" CREATED="1675592705508" MODIFIED="1675592716508">
+<node TEXT="Reason" ID="ID_769299878" CREATED="1675592719437" MODIFIED="1675592722249">
+<node TEXT="Easier to debug" ID="ID_1428289559" CREATED="1675601606327" MODIFIED="1675601610447">
+<node TEXT="On CI/CD environment, browser doesn&apos;t always opened" ID="ID_1945447863" CREATED="1675592722932" MODIFIED="1675592747318"/>
+</node>
+</node>
+</node>
+<node TEXT="Always set display resolution" ID="ID_469666329" CREATED="1675594416497" MODIFIED="1675594433722">
+<node TEXT="Capabilities" ID="ID_1189746622" CREATED="1675594409128" MODIFIED="1675594415645">
+<node TEXT="Chrome (Headless)" ID="ID_1976080185" CREATED="1675594452620" MODIFIED="1675594457797">
+<node TEXT="args: List" ID="ID_1826646246" CREATED="1675594445029" MODIFIED="1675594725076">
+<node TEXT="&quot;window-size=1920,1080&quot;" ID="ID_1704281718" CREATED="1675594472267" MODIFIED="1675595026446"/>
+</node>
+</node>
+</node>
+<node TEXT="Reason" ID="ID_1368929789" CREATED="1675594533082" MODIFIED="1675594536575">
+<node TEXT="Chrome (Headless)" ID="ID_1148924621" CREATED="1675594540357" MODIFIED="1675594549430">
+<node TEXT="Server&apos;s desktop resolution usually has lower resolution than tester laptop" ID="ID_323108550" CREATED="1675594564526" MODIFIED="1675594593331"/>
+<node TEXT="Usually run in low resolution" ID="ID_1119608660" CREATED="1675594549796" MODIFIED="1675594563877"/>
+</node>
+</node>
+</node>
+<node TEXT="while" ID="ID_251668291" CREATED="1675601635167" MODIFIED="1675601638458">
+<node TEXT="Always add count limit" ID="ID_1065587488" CREATED="1675601639076" MODIFIED="1675601669972">
+<node TEXT="No element could be assured to fulfil the expectation all the time" ID="ID_482343947" CREATED="1675646373694" MODIFIED="1675646435639"/>
+</node>
+</node>
+<node TEXT="Mark/separate tests that not allowed to run in production environment" ID="ID_988283653" CREATED="1675733373929" MODIFIED="1675733597500">
+<node TEXT="Use different test suite" ID="ID_561873286" CREATED="1675733677562" MODIFIED="1675733686459">
+<node TEXT="Branching in groovy code is hard to maintained" ID="ID_1237985245" CREATED="1675733618931" MODIFIED="1675733668127"/>
+</node>
+</node>
+</node>
+<node TEXT="Risk" ID="ID_1438843142" CREATED="1675601425302" MODIFIED="1675601432213">
+<node TEXT="Minimizing" ID="ID_1027287073" CREATED="1675601435526" MODIFIED="1675601443299">
+<node TEXT="For money, always the smallest value as possible" ID="ID_783168566" CREATED="1675595076629" MODIFIED="1675595102755">
+<node TEXT="Minimizing loss, sometimes you don&apos;t know the test get run on production" ID="ID_318949604" CREATED="1675733721133" MODIFIED="1675733894619"/>
+</node>
+<node TEXT="Always encrypt password" ID="ID_266820993" CREATED="1675595056090" MODIFIED="1675595062458"/>
+<node TEXT="Profile" ID="ID_123810669" CREATED="1675595118834" MODIFIED="1675595180484">
+<node TEXT="Never use Default profile" ID="ID_1521861387" CREATED="1675595129351" MODIFIED="1675595144214"/>
+<node TEXT="Always use environment specific named test data" ID="ID_650822202" CREATED="1675595170228" MODIFIED="1675595187045">
+<node TEXT="eg" ID="ID_383713331" CREATED="1675595188135" MODIFIED="1675595189893">
+<node TEXT="users-dev.csv" ID="ID_1536951292" CREATED="1675595190286" MODIFIED="1675595210504"/>
+</node>
+</node>
+<node TEXT="If profile is in same repository with katalon project" ID="ID_208030769" CREATED="1675595269381" MODIFIED="1675595289344">
+<node TEXT="Always use environment specific named profile" ID="ID_592504135" CREATED="1675595145981" MODIFIED="1675595176449">
+<node TEXT="eg" ID="ID_1281740770" CREATED="1675595216098" MODIFIED="1675595217111">
+<node TEXT="Development" ID="ID_1674209962" CREATED="1675595217881" MODIFIED="1675595228824"/>
+<node TEXT="my-prod.co.id" ID="ID_313443045" CREATED="1675595230019" MODIFIED="1675595242833"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Repository" ID="ID_855768251" CREATED="1675733425586" MODIFIED="1675733431986">
+<node TEXT="Consider to use different repository for" ID="ID_1882241565" CREATED="1675733433279" MODIFIED="1675733451003">
+<node TEXT="Profile" ID="ID_1855529761" CREATED="1675733451644" MODIFIED="1675733461458">
+<node TEXT="Per target environment" ID="ID_1191728234" CREATED="1675733483641" MODIFIED="1675733492391"/>
+</node>
+<node TEXT="Test data" ID="ID_69955872" CREATED="1675733466525" MODIFIED="1675733469651">
+<node TEXT="Per target environment" ID="ID_1307562832" CREATED="1675733483641" MODIFIED="1675733492391"/>
+</node>
+<node TEXT="Katalon project" ID="ID_768355213" CREATED="1675733474678" MODIFIED="1675733479112"/>
+</node>
+</node>
+<node TEXT="Test data" ID="ID_59308986" CREATED="1675733792929" MODIFIED="1675733796591">
+<node TEXT="Whenever posible do automated" ID="ID_430029930" CREATED="1675733797258" MODIFIED="1675733815729">
+<node TEXT="Set-up" ID="ID_892385151" CREATED="1675733816263" MODIFIED="1675733819041"/>
+<node TEXT="Clean up" ID="ID_933260856" CREATED="1675733819376" MODIFIED="1675733824756"/>
+</node>
+</node>
+</node>
+</node>
+</map>
